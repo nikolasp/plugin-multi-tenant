7de8ede9965a379e0f3ada28e9b2dcf69f142649
Nikola Spalevic <nikolaspalevic@gmail.com>
2025-02-15 13:31:59 +0100

    v3.23.0

--- dist/utilities/addFilterOptionsToFields.d.ts
+++ dist/utilities/addFilterOptionsToFields.d.ts
@@ -1,10 +1,17 @@
-import type { Field } from 'payload';
+import type { Config, Field, SanitizedConfig } from "payload";
 type AddFilterOptionsToFieldsArgs = {
+    config: (Config | SanitizedConfig) & {
+        blocks?: {
+            slug: string;
+            fields: Field[];
+            [key: string]: unknown;
+        }[];
+    };
     fields: Field[];
     tenantEnabledCollectionSlugs: string[];
     tenantEnabledGlobalSlugs: string[];
     tenantFieldName: string;
     tenantsCollectionSlug: string;
 };
-export declare function addFilterOptionsToFields({ fields, tenantEnabledCollectionSlugs, tenantEnabledGlobalSlugs, tenantFieldName, tenantsCollectionSlug, }: AddFilterOptionsToFieldsArgs): void;
+export declare function addFilterOptionsToFields({ config, fields, tenantEnabledCollectionSlugs, tenantEnabledGlobalSlugs, tenantFieldName, tenantsCollectionSlug, }: AddFilterOptionsToFieldsArgs): void;
 export {};
