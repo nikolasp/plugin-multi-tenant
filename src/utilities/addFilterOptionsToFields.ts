0aaecd39b793b2177c7c4533b7ee685dcea1c3ed
Nikola Spalevic <nikolaspalevic@gmail.com>
2025-02-21 09:49:23 +0100

    v3.24.0

--- src/utilities/addFilterOptionsToFields.ts
+++ src/utilities/addFilterOptionsToFields.ts
@@ -1,17 +1,28 @@
-import type { Field, FilterOptionsProps, RelationshipField } from 'payload'
+import type {
+  BlocksField,
+  Config,
+  Field,
+  FilterOptionsProps,
+  RelationshipField,
+  SanitizedConfig,
+} from "payload";
 
-import { getCollectionIDType } from './getCollectionIDType'
-import { getTenantFromCookie } from './getTenantFromCookie'
+import { getCollectionIDType } from "./getCollectionIDType";
+import { getTenantFromCookie } from "./getTenantFromCookie";
 
 type AddFilterOptionsToFieldsArgs = {
-  fields: Field[]
-  tenantEnabledCollectionSlugs: string[]
-  tenantEnabledGlobalSlugs: string[]
-  tenantFieldName: string
-  tenantsCollectionSlug: string
-}
+  config: (Config | SanitizedConfig) & {
+    blocks?: { slug: string; fields: Field[]; [key: string]: unknown }[];
+  };
+  fields: Field[];
+  tenantEnabledCollectionSlugs: string[];
+  tenantEnabledGlobalSlugs: string[];
+  tenantFieldName: string;
+  tenantsCollectionSlug: string;
+};
 
 export function addFilterOptionsToFields({
+  config,
   fields,
   tenantEnabledCollectionSlugs,
   tenantEnabledGlobalSlugs,
@@ -19,26 +30,31 @@ export function addFilterOptionsToFields({
   tenantsCollectionSlug,
 }: AddFilterOptionsToFieldsArgs) {
   fields.forEach((field) => {
-    if (field.type === 'relationship') {
+    if (field.type === "relationship") {
       /**
        * Adjusts relationship fields to filter by tenant
        * and ensures relationTo cannot be a tenant global collection
        */
-      if (typeof field.relationTo === 'string') {
+      if (typeof field.relationTo === "string") {
         if (tenantEnabledGlobalSlugs.includes(field.relationTo)) {
           throw new Error(
-            `The collection ${field.relationTo} is a global collection and cannot be related to a tenant enabled collection.`,
-          )
+            `The collection ${field.relationTo} is a global collection and cannot be related to a tenant enabled collection.`
+          );
         }
         if (tenantEnabledCollectionSlugs.includes(field.relationTo)) {
-          addFilter({ field, tenantEnabledCollectionSlugs, tenantFieldName, tenantsCollectionSlug })
+          addFilter({
+            field,
+            tenantEnabledCollectionSlugs,
+            tenantFieldName,
+            tenantsCollectionSlug,
+          });
         }
       } else {
         field.relationTo.map((relationTo) => {
           if (tenantEnabledGlobalSlugs.includes(relationTo)) {
             throw new Error(
-              `The collection ${relationTo} is a global collection and cannot be related to a tenant enabled collection.`,
-            )
+              `The collection ${relationTo} is a global collection and cannot be related to a tenant enabled collection.`
+            );
           }
           if (tenantEnabledCollectionSlugs.includes(relationTo)) {
             addFilter({
@@ -46,59 +62,73 @@ export function addFilterOptionsToFields({
               tenantEnabledCollectionSlugs,
               tenantFieldName,
               tenantsCollectionSlug,
-            })
+            });
           }
-        })
+        });
       }
     }
 
     if (
-      field.type === 'row' ||
-      field.type === 'array' ||
-      field.type === 'collapsible' ||
-      field.type === 'group'
+      field.type === "row" ||
+      field.type === "array" ||
+      field.type === "collapsible" ||
+      field.type === "group"
     ) {
       addFilterOptionsToFields({
+        config,
         fields: field.fields,
         tenantEnabledCollectionSlugs,
         tenantEnabledGlobalSlugs,
         tenantFieldName,
         tenantsCollectionSlug,
-      })
+      });
     }
 
-    if (field.type === 'blocks') {
-      field.blocks.forEach((block) => {
-        addFilterOptionsToFields({
-          fields: block.fields,
-          tenantEnabledCollectionSlugs,
-          tenantEnabledGlobalSlugs,
-          tenantFieldName,
-          tenantsCollectionSlug,
-        })
-      })
+    if (field.type === "blocks") {
+      (
+        (field as BlocksField & { blockReferences: string[] })
+          .blockReferences ?? field.blocks
+      ).forEach((_block) => {
+        const block =
+          typeof _block === "string"
+            ? // TODO: iterate over blocks mapped to block slug in v4, or pass through payload.blocks
+              config?.blocks?.find((b) => b.slug === _block)
+            : _block;
+
+        if (block?.fields) {
+          addFilterOptionsToFields({
+            config,
+            fields: block.fields,
+            tenantEnabledCollectionSlugs,
+            tenantEnabledGlobalSlugs,
+            tenantFieldName,
+            tenantsCollectionSlug,
+          });
+        }
+      });
     }
 
-    if (field.type === 'tabs') {
+    if (field.type === "tabs") {
       field.tabs.forEach((tab) => {
         addFilterOptionsToFields({
+          config,
           fields: tab.fields,
           tenantEnabledCollectionSlugs,
           tenantEnabledGlobalSlugs,
           tenantFieldName,
           tenantsCollectionSlug,
-        })
-      })
+        });
+      });
     }
-  })
+  });
 }
 
 type AddFilterArgs = {
-  field: RelationshipField
-  tenantEnabledCollectionSlugs: string[]
-  tenantFieldName: string
-  tenantsCollectionSlug: string
-}
+  field: RelationshipField;
+  tenantEnabledCollectionSlugs: string[];
+  tenantFieldName: string;
+  tenantsCollectionSlug: string;
+};
 function addFilter({
   field,
   tenantEnabledCollectionSlugs,
@@ -106,19 +136,24 @@ function addFilter({
   tenantsCollectionSlug,
 }: AddFilterArgs) {
   // User specified filter
-  const originalFilter = field.filterOptions
+  const originalFilter = field.filterOptions;
   field.filterOptions = async (args) => {
     const originalFilterResult =
-      typeof originalFilter === 'function' ? await originalFilter(args) : (originalFilter ?? true)
+      typeof originalFilter === "function"
+        ? await originalFilter(args)
+        : (originalFilter ?? true);
 
     // If the relationTo is not a tenant enabled collection, return early
-    if (args.relationTo && !tenantEnabledCollectionSlugs.includes(args.relationTo)) {
-      return originalFilterResult
+    if (
+      args.relationTo &&
+      !tenantEnabledCollectionSlugs.includes(args.relationTo)
+    ) {
+      return originalFilterResult;
     }
 
     // If the original filtr returns false, return early
     if (originalFilterResult === false) {
-      return false
+      return false;
     }
 
     // Custom tenant filter
@@ -126,40 +161,40 @@ function addFilter({
       ...args,
       tenantFieldName,
       tenantsCollectionSlug,
-    })
+    });
 
     // If the tenant filter returns true, just use the original filter
     if (tenantFilterResults === true) {
-      return originalFilterResult
+      return originalFilterResult;
     }
 
     // If the original filter returns true, just use the tenant filter
     if (originalFilterResult === true) {
-      return tenantFilterResults
+      return tenantFilterResults;
     }
 
     return {
       and: [originalFilterResult, tenantFilterResults],
-    }
-  }
+    };
+  };
 }
 
 type Args = {
-  tenantFieldName?: string
-  tenantsCollectionSlug: string
-} & FilterOptionsProps
+  tenantFieldName?: string;
+  tenantsCollectionSlug: string;
+} & FilterOptionsProps;
 const filterOptionsByTenant = ({
   req,
-  tenantFieldName = 'tenant',
+  tenantFieldName = "tenant",
   tenantsCollectionSlug,
 }: Args) => {
   const idType = getCollectionIDType({
     collectionSlug: tenantsCollectionSlug,
     payload: req.payload,
-  })
-  const selectedTenant = getTenantFromCookie(req.headers, idType)
+  });
+  const selectedTenant = getTenantFromCookie(req.headers, idType);
   if (!selectedTenant) {
-    return true
+    return true;
   }
 
   return {
@@ -177,5 +212,5 @@ const filterOptionsByTenant = ({
         },
       },
     ],
-  }
-}
+  };
+};
